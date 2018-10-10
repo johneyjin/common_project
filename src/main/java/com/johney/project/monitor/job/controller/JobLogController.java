ad73a79e3a8932f2627cd8ba09a704bab206963b
johney <johney_jin@163.com>
2018-10-10 11:43:19 +0800

    初次提交

---
+++ src/main/java/com/johney/project/monitor/job/controller/JobLogController.java
@@ -0,0 +1,72 @@
+package com.johney.project.monitor.job.controller;
+
+import java.util.List;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.johney.common.utils.poi.ExcelUtil;
+import com.johney.framework.aspectj.lang.annotation.Log;
+import com.johney.framework.aspectj.lang.enums.BusinessType;
+import com.johney.framework.web.controller.BaseController;
+import com.johney.framework.web.domain.AjaxResult;
+import com.johney.framework.web.page.TableDataInfo;
+import com.johney.project.monitor.job.domain.JobLog;
+import com.johney.project.monitor.job.service.IJobLogService;
+
+/**
+ * 调度日志操作处理
+ * 
+ * @author ruoyi
+ */
+@Controller
+@RequestMapping("/monitor/jobLog")
+public class JobLogController extends BaseController
+{
+    private String prefix = "monitor/job";
+
+    @Autowired
+    private IJobLogService jobLogService;
+
+    @RequiresPermissions("monitor:job:view")
+    @GetMapping()
+    public String jobLog()
+    {
+        return prefix + "/jobLog";
+    }
+
+    @RequiresPermissions("monitor:job:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(JobLog jobLog)
+    {
+        startPage();
+        List<JobLog> list = jobLogService.selectJobLogList(jobLog);
+        return getDataTable(list);
+    }
+
+    @Log(title = "调度日志", businessType = BusinessType.EXPORT)
+    @RequiresPermissions("monitor:job:export")
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(JobLog jobLog)
+    {
+        List<JobLog> list = jobLogService.selectJobLogList(jobLog);
+        ExcelUtil<JobLog> util = new ExcelUtil<JobLog>(JobLog.class);
+        return util.exportExcel(list, "jobLog");
+    }
+
+    @Log(title = "调度日志", businessType = BusinessType.DELETE)
+    @RequiresPermissions("monitor:job:remove")
+    @PostMapping("/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids)
+    {
+        return toAjax(jobLogService.deleteJobLogByIds(ids));
+    }
+}
